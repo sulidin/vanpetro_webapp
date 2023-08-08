a724407e3ef3ac05dc690d1123b9157d85030113
Furkan BOZOKLUOGLU <furkan@Furkans-MacBook-Air-2.local>
2023-08-08 10:30:47 -0700

    fix typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import '@styles/globals.css'
 import '@styles/Style.css'
 import Navbar from '@components/Navbar'
 import Provider from '@components/Provider'
-import Footer from '@components/footer'
+import Footer from '@components/Footer'
 
 
 
